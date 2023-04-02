0e24c09f7b9e59bf4728f0dbef05eef9a70bba5a
isaeed <isaeed@macrosoftinc.com>
2023-04-02 17:11:56 +0500

    Release 3.4.0

---
+++ Urdu (ur-PK)/Oqtane.Client/Resources/Oqtane.Framework.ur-PK.cmd
@@ -0,0 +1,5 @@
+dotnet build -c Release ..\..\Oqtane.sln
+mkdir ..\..\Oqtane.Server\bin\Debug\net6.0\ur-PK
+copy ..\..\Oqtane.Server\bin\Release\net6.0\ur-PK\Oqtane.Client.resources.dll ..\..\Oqtane.Server\bin\Debug\net6.0\ur-PK\Oqtane.Client.resources.dll
+..\..\Oqtane.Package\nuget.exe pack Oqtane.Framework.ur-PK.nuspec
+pause 
